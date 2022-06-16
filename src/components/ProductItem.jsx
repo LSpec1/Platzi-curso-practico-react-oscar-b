722e0ab567af7eae19aa4b9d4c87fd2c48fd70bb
Elian <toroelian4@gmail.com>
2022-06-16 19:57:00 -0400

    Clase 16

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../styles/ProductItem.scss';
 
-const ProductItem = () => {
+import cart from '../assets/icons/bt_add_to_cart.svg'
+
+const ProductItem = (props) => {
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${props.price}</p>
+					<p>{props.name}</p>
 				</div>
 				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+					<img src={cart} alt="" />
 				</figure>
 			</div>
 		</div>
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -6,7 +6,12 @@ const ProductList = () => {
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				<ProductItem name="Bike" price="120,00"/>
+				<ProductItem name="Bike" price="20,00"/>
+				<ProductItem name="Bike" price="520,00"/>
+				<ProductItem name="Bike" price="320,00"/>
+				<ProductItem name="Bike" price="10,00"/>
+				<ProductItem name="Bike" price="220,00"/>
 			</div>
 		</section>
 	);
