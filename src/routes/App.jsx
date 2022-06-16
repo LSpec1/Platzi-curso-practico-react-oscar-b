477be5c5605ca96800258f4d21ba81c18fae8f44
Elian <toroelian4@gmail.com>
2022-06-15 20:17:35 -0400

    Clase 10 React

---
+++ src/routes/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword'
+import '../styles/global.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path='login' element={<Login />} />
+            <Route exact path='recovery-password' element={<RecoveryPassword />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
