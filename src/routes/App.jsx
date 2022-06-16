85da69a888533b4c96d4f1412d0244ba07e2c331
Elian <toroelian4@gmail.com>
2022-06-16 18:04:51 -0400

    Clase 11

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword'
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 const App = () => {
