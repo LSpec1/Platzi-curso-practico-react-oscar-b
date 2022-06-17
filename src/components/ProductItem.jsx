f94f6e82e030e693b08fbccf9204f92f28c358e4
Elian <toroelian4@gmail.com>
2022-06-17 19:06:28 -0400

    Clase 17

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
 
-import cart from '../assets/icons/bt_add_to_cart.svg'
+import img_cart from '../assets/icons/bt_add_to_cart.svg'
+const imgBike = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 
 const ProductItem = (props) => {
+	const [cart, setCart] = useState(0);
+
+	const handleCart = () => {
+		setCart(cart+1);
+	}
+
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={imgBike} alt="" />
 			<div className="product-info">
 				<div>
 					<p>${props.price}</p>
 					<p>{props.name}</p>
 				</div>
-				<figure>
-					<img src={cart} alt="" />
+				<figure onClick={handleCart}>
+					<img src={img_cart} alt="" />
+					{cart}
 				</figure>
 			</div>
 		</div>
