99689a31a2f39885a796e015776113e91a12bb15
Elian <toroelian4@gmail.com>
2022-06-18 15:10:11 -0400

    Clase 22

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState, useContext } from 'react';
 import '../styles/Header.scss';
 import Menu from '../components/Menu'
+import AppContext from '../context/AppContext'
 
 import menu from '../assets/icons/icon_menu.svg';
 import logo from '../assets/logos/logo_yard_sale.svg'
 import shoppingCart from '../assets/icons/icon_shopping_cart.svg'
 
 const Header = () => {
-	const [toggle, setToggle] = useState(false);
+	const [ toggle, setToggle ] = useState(false);
+	const { state } = useContext(AppContext);
 
 	const handleToggle = () => {
 		setToggle(!toggle);
+	}
+	const mostrarItems = () => {
+		console.log(state.cart.length);
 	}
 
 	return (
@@ -45,8 +50,8 @@ const Header = () => {
 						platzi@example.com
 					</li>
 					<li className="navbar-shopping-cart">
-						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						<img src={shoppingCart} alt="shopping cart" onClick={mostrarItems}/>
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
